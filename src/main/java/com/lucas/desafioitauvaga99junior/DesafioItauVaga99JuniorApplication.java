0265ded10de1b446e50fd0c8d28de7ca1c5857e2
Lucas Alves <lucas0asm@outlook.com>
2026-09-20 20:56:11 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/lucas/desafioitauvaga99junior/DesafioItauVaga99JuniorApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.desafioitauvaga99junior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioItauVaga99JuniorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioItauVaga99JuniorApplication.class, args);
+    }
+
+}
