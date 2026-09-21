200c959d7f3d97748fa00c0356b655b79420709b
Lucas Alves <lucas0asm@outlook.com>
2026-09-21 15:25:04 -0300

    feat(exception): handle validation and business errors returning 422

---
+++ src/main/java/com/lucas/desafioitauvaga99junior/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.lucas.desafioitauvaga99junior.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_CONTENT)
+    public void tratarErroDeValidacao(MethodArgumentNotValidException exception) {
+    }
+}
