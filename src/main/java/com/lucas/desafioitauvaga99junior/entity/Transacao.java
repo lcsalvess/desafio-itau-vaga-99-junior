8fb89ca5f8e45e136c60ef06d1bf0c3545ab199c
Lucas Alves <lucas0asm@outlook.com>
2026-09-21 14:04:35 -0300

    feat(entity): add transacao and estatistica entity

---
+++ src/main/java/com/lucas/desafioitauvaga99junior/entity/Transacao.java
@@ -0,0 +1,23 @@
+package com.lucas.desafioitauvaga99junior.entity;
+
+import java.time.OffsetDateTime;
+
+public class Transacao {
+
+    private Double valor;
+    private OffsetDateTime dataHora;
+
+    public Transacao(Double valor, OffsetDateTime dataHora) {
+        this.valor = valor;
+        this.dataHora = dataHora;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public OffsetDateTime getDataHora() {
+        return dataHora;
+    }
+
+}
