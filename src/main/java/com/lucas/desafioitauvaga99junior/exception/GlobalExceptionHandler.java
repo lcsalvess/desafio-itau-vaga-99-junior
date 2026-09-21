fb3a8bd6d0b7cb95e21499aa73c515abc32eb4ca
Lucas Alves <lucas0asm@outlook.com>
2026-09-21 20:36:01 -0300

    fix: handle invalid request errors

--- src/main/java/com/lucas/desafioitauvaga99junior/exception/GlobalExceptionHandler.java
+++ src/main/java/com/lucas/desafioitauvaga99junior/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.lucas.desafioitauvaga99junior.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,5 +13,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.UNPROCESSABLE_CONTENT)
     public void tratarErroDeValidacao(MethodArgumentNotValidException exception) {
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void tratarErroDeRequisicaoInvalida(HttpMessageNotReadableException exception) {
     }
 }
