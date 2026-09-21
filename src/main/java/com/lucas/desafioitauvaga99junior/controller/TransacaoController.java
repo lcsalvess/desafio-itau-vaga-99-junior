a6cc583e5a4eaa7113cc2f388ff3ac034427060b
Lucas Alves <lucas0asm@outlook.com>
2026-09-21 15:07:45 -0300

    feat(controller): add POST /transacao endpoint

---
+++ src/main/java/com/lucas/desafioitauvaga99junior/controller/TransacaoController.java
@@ -0,0 +1,25 @@
+package com.lucas.desafioitauvaga99junior.controller;
+
+import com.lucas.desafioitauvaga99junior.dto.TransacaoDTO;
+import com.lucas.desafioitauvaga99junior.service.TransacaoService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/transacao")
+public class TransacaoController {
+
+    private final TransacaoService transacaoService;
+
+    public TransacaoController(TransacaoService transacaoService) {
+        this.transacaoService = transacaoService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void criar(@Valid @RequestBody TransacaoDTO dto) {
+        transacaoService.criar(dto);
+    }
+
+}
