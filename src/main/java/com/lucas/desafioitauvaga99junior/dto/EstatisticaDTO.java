dbe30d9257d57f0bf8fa9620e33e6d7b7e170b52
Lucas Alves <lucas0asm@outlook.com>
2026-09-21 14:59:20 -0300

    feat(dto): add static factory methods to EstatisticaDTO

--- src/main/java/com/lucas/desafioitauvaga99junior/dto/EstatisticaDTO.java
+++ src/main/java/com/lucas/desafioitauvaga99junior/dto/EstatisticaDTO.java
@@ -1,5 +1,7 @@
 package com.lucas.desafioitauvaga99junior.dto;
 
+import java.util.DoubleSummaryStatistics;
+
 public record EstatisticaDTO(
         long count,
         Double sum,
@@ -7,4 +9,17 @@ public record EstatisticaDTO(
         Double min,
         Double max
 ) {
+    public static EstatisticaDTO vazio() {
+        return new EstatisticaDTO(0L, 0.0, 0.0, 0.0, 0.0);
+    }
+
+    public static EstatisticaDTO aPartirDe(DoubleSummaryStatistics stats) {
+        return new EstatisticaDTO(
+                stats.getCount(),
+                stats.getSum(),
+                stats.getAverage(),
+                stats.getMin(),
+                stats.getMax()
+        );
+    }
 }
